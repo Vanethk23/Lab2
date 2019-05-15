efc6a0690db2a42a7d2b8ea8ac50860e4f82620d
Vanethk23 <46509339+Vanethk23@users.noreply.github.com>
2019-05-15 13:18:19 -0500

    Add files via upload

---
+++ Lab2/index.js
@@ -0,0 +1,6 @@
+const data = require('./top_2018_movies.json');
+const http= require('http');
+const fs= require('fs');
+console.log(data[0].all_movies);
+console.log(data[0].action);
+console.log(data[0].pg);
